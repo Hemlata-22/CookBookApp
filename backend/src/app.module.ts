62785e292012349f18ce51e44957a7eaddab6685
Hemlata-22 <hemlatarawat295@gmail.com>
2025-10-07 22:36:26 +0530

    Setup PostgreSQL with Docker and connected NestJS backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'cookbook_user',
+      password: 'cookbook_pass',
+      database: 'cookbook_db',
+      autoLoadEntities: true,
+      synchronize: true, // automatically creates tables from entities
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
