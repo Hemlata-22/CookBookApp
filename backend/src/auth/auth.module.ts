1735448ff392079ce7ada75e601cea7c355b74d5
Hemlata-22 <hemlatarawat295@gmail.com>
2025-10-14 22:31:17 +0530

    credential changes

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,13 +3,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    // Make sure ConfigModule is loaded to read .env variables
+    ConfigModule.forRoot({ isGlobal: true }),
+
     UserModule,
-    JwtModule.register({
-      secret: 'YOUR_SECRET_KEY',
+
+    JwtModule.registerAsync({
+      imports:[ConfigModule],
+      useFactory : async (configService: ConfigService) => ({
+      secret: configService.get<string>('JWT_SECRET'),
       signOptions: {expiresIn: '1h'},
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService],
